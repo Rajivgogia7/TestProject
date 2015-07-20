561fd3bd428da8d84df9a5e3cc334c288f3cd49c
Rajivgogia7 <Rajivgogia7@gmail.com>
2015-07-20 15:54:41 +0530

    11

--- GitHub_POC/Account/Login.aspx.cs
+++ GitHub_POC/Account/Login.aspx.cs
@@ -12,7 +12,7 @@ namespace GitHub_POC.Account
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            int a = 10000;
+            int a = 11;
             RegisterHyperLink.NavigateUrl = "Register";
             OpenAuthLogin.ReturnUrl = Request.QueryString["ReturnUrl"];
 
