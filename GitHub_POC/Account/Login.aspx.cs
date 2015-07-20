9c428f937a7ebea7076a701661b12fb46ddc3ced
Rajivgogia7 <Rajivgogia7@gmail.com>
2015-07-20 15:17:27 +0530

    change test 1
    Signed-off-by: Rajivgogia7 <Rajivgogia7@gmail.com>

--- GitHub_POC/Account/Login.aspx.cs
+++ GitHub_POC/Account/Login.aspx.cs
@@ -12,7 +12,7 @@ namespace GitHub_POC.Account
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            int a = 10;
+            int a = 100;
             RegisterHyperLink.NavigateUrl = "Register";
             OpenAuthLogin.ReturnUrl = Request.QueryString["ReturnUrl"];
 
